bd1ec84ce7915cd71d98c4b878259b4240f475c5
babusswaine <124659900+babusswaine@users.noreply.github.com>
2025-01-14 15:22:56 +0800

    Update IntroductionSection to prevent 'e-commerce' word wrap (#5)

--- apps/astro/src/components/home/IntroductionSection.tsx
+++ apps/astro/src/components/home/IntroductionSection.tsx
@@ -1,7 +1,7 @@
 const IntroductionSection = () => {
   return (
     <div className="flex h-full w-full flex-col justify-center gap-10 px-8">
-      <div className="font-serif text-2xl text-foreground lg:text-3xl">
+      <div className="mx-auto max-w-lg font-serif text-2xl text-foreground lg:text-3xl">
         <p>Hi, I’m Leon!</p>
         <br />
         <p>
@@ -10,13 +10,13 @@ const IntroductionSection = () => {
         </p>
         <br />
         <p>
-          With over five years of experience in retail and e-commerce, I thrive
-          on collaboration and crafting intuitive solutions that make life
-          easier for people every day.
+          With over five years of experience in retail and{" "}
+          <span className="whitespace-nowrap">e-commerce</span>, I thrive on
+          collaboration and crafting intuitive solutions that make life easier
+          for people every day.
         </p>
       </div>
     </div>
   );
 };
-
 export default IntroductionSection;
